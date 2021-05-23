529b6ad46dfe3821c0e7b3387df707114b37aebf
Joel Löfgren <jdtlofgren@gmail.com>
2021-05-23 14:40:42 +0100

    Added comments and annotations

--- src/Adventure/AdventureData.php
+++ src/Adventure/AdventureData.php
@@ -4,60 +4,97 @@ declare(strict_types=1);
 
 namespace App\Adventure;
 
+/**
+ * A class holding the game data
+ */
 class AdventureData
 {
+    /**
+    * @var array $data Holds the game data
+    */
     private array $data;
 
     public function __construct()
     {
     }
 
+    /**
+     * Sets data for the roomdescription in $data
+     * @return void
+     */
     public function setPlayerRoomDescriptionData(string $description): void
     {
         $this->data['playerRoomDescription'] = $description;
     }
 
+    /**
+     * Sets data for the room image in $data
+     * @return void
+     */
     public function setPlayerRoomImageData(string $roomImage): void
     {
         $this->data['playerRoomImage'] = $roomImage;
     }
 
+    /**
+     * Sets data for the room name in $data
+     * @return void
+     */
     public function setPlayerRoomNameData(string $roomName): void
     {
         $this->data['playerRoomName'] = $roomName;
     }
 
-    /* public function setPlayerRoomExitsNameData(array $exitNames): void
-    {
-        $this->data['playerRoomExitNameData'] = $exitNames;
-    } */
-
+    /**
+     * Sets data for the room exits in $data
+     * @return void
+     */
     public function setPlayerRoomExits(array $exits): void
     {
         $this->data['playerRoomExits'] = $exits;
     }
 
+    /**
+     * Sets data for the room items in $data
+     * @return void
+     */
     public function setPlayerRoomItemsData(array $roomItems): void
     {
         $this->data['playerRoomItems'] = $roomItems;
     }
 
+    /**
+     * Sets data for players items in $data
+     * @return void
+     */
     public function setPlayerItemsData(array $playerItems): void
     {
         $this->data['playerItems'] = $playerItems;
     }
 
+    /**
+     * Sets data for the room index in $data
+     * @return void
+     */
     public function setPlayerRoomIndexData(string $currentRoomIndex): void
     {
         $this->data['playerRoomIndex'] = $currentRoomIndex;
     }
 
+    /**
+     * Sets data for the temporary descriptions (events) in $data
+     * @return void
+     */
     public function setPlayerRoomTempDescriptionsData(array $tempDescriptions): void
     {
         $this->data['playerRoomTempDescriptions'] = $tempDescriptions;
     }
 
-    public function getAdventureData()
+    /**
+     * Gets $data
+     * @return array the $data array
+     */
+    public function getAdventureData(): array
     {
         return $this->data;
     }
