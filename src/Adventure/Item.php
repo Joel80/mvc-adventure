074db541ac7b61c31862d6bb34bd1ff5808d8193
Joel Löfgren <jdtlofgren@gmail.com>
2021-05-23 15:26:23 +0100

    Added comments and annotations

--- src/Adventure/Item.php
+++ src/Adventure/Item.php
@@ -10,6 +10,8 @@ class Item
     /**
      * @var string $description the item description
      * @var string $name the name of the item
+     * @var string $itemId the item id
+     * @var string $placementDescription the description of the items placement
      */
 
     private string $description;
@@ -17,6 +19,13 @@ class Item
     private string $itemId;
     private string $placementDescription;
 
+    /**
+     * Constructor
+     * @param string $description the item description
+     * @param string $name the name of the item
+     * @param string $itemId the item id
+     * @param string $placementDescription the description of the items placement
+     */
     public function __construct(string $description, string $name, string $itemId, string $itemPlacement)
     {
         $this->description = $description;
@@ -25,21 +34,37 @@ class Item
         $this->placementDescription = $itemPlacement;
     }
 
+    /**
+     * Gets the item description
+     * @return string the description
+     */
     public function getDescription(): string
     {
         return $this->description;
     }
 
+    /**
+     * Gets the placement description
+     * @return string the description
+     */
     public function getPlacementDescription(): string
     {
         return $this->placementDescription;
     }
 
+    /**
+     * Gets the item name
+     * @return string the name
+     */
     public function getName(): string
     {
         return $this->name;
     }
 
+    /**
+     * Gets the item id
+     * @return string the id
+     */
     public function getItemId(): string
     {
         return $this->itemId;
